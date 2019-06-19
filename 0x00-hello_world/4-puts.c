82bb16723b033c49ca42259787e7463719df3ea5
JoseMarulanda <josejumarumol@gmail.com>
2019-06-19 20:49:32 +0000

    4 fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("Programing is like building a multilingual puzzle\n");
-getchar ();
 return (0);
 }
