24987ff8faecc32f7e511b98b0fdef2c74a96f88
JoseMarulanda <josejumarumol@gmail.com>
2019-06-28 00:50:04 +0000

    fix trailing whitspaces

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  * more_numbers - prints 10 times the numbers, from 0 to 14
- * 
+ *
  * Return: no return.
  */
 void more_numbers(void)
@@ -17,6 +17,6 @@ _putchar(j / 10 + '0');
 _putchar(j % 10 + '0');
 }
 _putchar('\n');
-j= 0;
+j = 0;
 }
 }
