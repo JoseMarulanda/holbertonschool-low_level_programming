3a65365e0680e885e0aaa3e5df258dd622a35085
JoseMarulanda <josejumarumol@gmail.com>
2020-01-16 23:04:45 +0000

    finished tasks

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,20 @@
+#include "search_algos.h"
+/**
+ * linear_search - Linear search algorithm
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search
+ * Return: index if found or -1 not found
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%i]", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
