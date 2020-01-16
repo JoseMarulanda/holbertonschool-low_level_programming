ef56fbf4a0d63e41a790dabbdf5dadedac75dcb6
JoseMarulanda <josejumarumol@gmail.com>
2020-01-16 23:13:55 +0000

    added missing new line

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,7 +12,7 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%lu] = [%i]", i, array[i]);
+		printf("Value checked array[%lu] = [%i] \n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
